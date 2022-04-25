a937d599fef969a05869c98e9e9c553595740c60
Owzaa <nyondo.olwethu@gmail.comm>
2022-04-25 16:48:55 +0200

    commit master

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import SearchForm from './components/SearchForm/searchForm';
 import Footer from './pages/Footer/Footer';
+import Home from './pages/Home/Home';
 
 
 function App() {
@@ -13,17 +14,19 @@ function App() {
   return (
     <div className="App">
 
-
     <div className='table-Container'>
       <SearchForm/>
-
-
     </div>
 
+<section className='homeView-Container'>
 
+<div className='container'>
+  <Home/>
+</div>
 
-   <section className='footer'>
+</section>
 
+   <section className='footer-container'>
      <Footer/>
    </section>
    
