2caade7ac36ac6cd24119f8cef6c7db3318154b3
Owzaa <nyondo.olwethu@gmail.comm>
2022-04-25 10:11:50 +0200

    commit on master

--- src/features/Reducers/CountryReducer.tsx
+++ src/features/Reducers/CountryReducer.tsx
@@ -1,12 +1,38 @@
-
+//
 const initState={
     countries:[],
     isLoading:false,
     error:''
 }
 
-export default function countryReducer(state=initState){
+export default function countryReducer(state=initState,action:any){
 
+    switch(action.type){
 
+// API fetch country data[] isLoading === true
+case'FETCH_COUNTRIES':
+    return{
+        ...state,
+        isLoading:true
+}
+
+// If fetching is Succesfull
+case 'FETCG_COUNTRIES_SUCCESS':
+    return{
+        ...state,
+        isLoading:false,
+        countries:action.payload,
+        error:'',
+    }
+// if fetching has ERRORS
+case 'FETCH_COUNTRIES_FAILURE':
+    return{
+        ...state,
+        isLoading:false,
+        error:action.payload 
+    }
+        default:
+            return state
     
+    }
 }
